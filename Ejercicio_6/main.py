d458f7be377f05f98da914022697fd825af1e156
Gabriel09842 <157264815+Gabriel09842@users.noreply.github.com>
2024-02-09 04:25:13 +0000

    Ejercicios PC3

---
+++ Ejercicio_6/main.py
@@ -0,0 +1,11 @@
+from producto import Producto, Catalogo
+
+catalogo = Catalogo()
+
+catalogo.agregar_producto(Producto("Llanta", "001", 100, 2022))
+catalogo.agregar_producto(Producto("Filtro de aceite", "002", 15, 2023))
+catalogo.agregar_producto(Producto("Batería", "003", 150, 2022))
+
+catalogo.mostrar_productos()
+
+catalogo.filtrar_por_año(2022)
